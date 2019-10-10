d369685c82757ee2aaf118703f6ca82a7fb63eb4
Duncan Macleod <duncan.macleod@ligo.org>
2019-10-10 15:27:41 +0100

    updated recipe
    to remove overlinking from gsl and cfitsio

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 
-./configure \
+# only link libraries we actually use
+export CFITSIO_LIBS="-L${PREFIX}/lib -lcfitsio"
+export GSL_LIBS="-L${PREFIX}/lib -lgsl"
+
+mkdir -p _build
+pushd _build
+
+# configure
+${SRC_DIR}/configure \
 	--prefix=${PREFIX} \
 	--disable-gcc-flags \
 	--enable-cfitsio \
@@ -10,6 +18,11 @@
 	MPICXX=${PREFIX}/bin/mpicxx \
 	MPIFC=${PREFIX}/bin/mpifc
 
+# build
 make -j ${CPU_COUNT}
+
+# check
 make -j ${CPU_COUNT} check
+
+# install
 make -j ${CPU_COUNT} install
